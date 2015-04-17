06e7b65620072c96fcc2816bc40424fb36d56b3a
celador@github.com <john.aaron.nelson@gmail.com>
2015-04-17 00:29:22 -0400

    Modified Provider per Style Guidelines

--- dist/jsr-mocks.provider.js
+++ dist/jsr-mocks.provider.js
@@ -1,141 +1,139 @@
-'use strict';
-
-angular.module('jsrMocks', [])
-    .provider('jsrMocks', jsrMocks)
-    .factory('jsr',jsr);
+(function() {
+    'use strict';
 
 
+    angular.module('jsrMocks', [])
+        .provider('jsrMocks', ['$window', jsrMocks])
+        .factory('jsr', ['jsrMocks', '$q', '$rootScope', jsr]);
 
-function jsrMocks() {
 
-    var $mocks;
+    var jsrMocks = function ($window) {
 
-    return {
+        var $mocks;
 
-        setMocks: function(mocks) {
-            $mocks = mocks;
-        },
+        return {
 
-        $get: function($log,$http) {
-            if(!window.Visualforce){
-                var mocker,
-                    mockType = typeof($mocks);
-                if(mockType === 'object'){
-                    mocker = invokeStaticAction;
-                    
-                }else{
-                    
-                    mocker = invokeHTTPAction;
-                }
-                    
-                return {
-                    remoting: {
-                        Manager: {
-                            invokeAction: mocker
-                        }
+            setMocks: function(mocks) {
+                $mocks = mocks;
+            },
+
+            $get: function($log, $http) {
+                var invokeStaticAction = function() {
+                    $log.debug('$mocks is an object:', $mocks);
+                    var lastArg = arguments[arguments.length - 1],
+                        callback = lastArg,
+                        mock = $mocks[arguments[0]],
+                        result = mock.method(arguments),
+                        event = {
+                            status: true
+                        };
+                    if (typeof(callback) === 'object') {
+                        callback = arguments[arguments.length - 2];
                     }
+                    setTimeout(function() {
+                        callback(result, event);
+                    }, mock.timeout);
                 };
-                
-            }else{
-                return Visualforce;
-            }
 
-            function invokeStaticAction(){
-                $log.debug('$mocks is an object:', $mocks);
-                var lastArg = arguments[arguments.length - 1],
-                    callback = lastArg,
-                    mock = $mocks[arguments[0]],
-                    result = mock.method(arguments),
-                    event = {
+                var invokeHTTPAction = function() {
+                    $log.debug('$mocks is not an object:', mockType, $mocks);
+                    var lastArg = arguments[arguments.length - 1];
+                    var callback = lastArg;
+                    var mockName = arguments[0];
+                    var regex = /(\w*)?}/;
+                    var match = regex.exec(mockName);
+
+                    mockName = match[0].split('\}')[0];
+
+                    var url = $mocks + mockName;
+                    var event = {
                         status: true
                     };
-                if (typeof(callback) === 'object') {
-                    callback = arguments[arguments.length - 2];
-                }
-                setTimeout(function() {
-                    callback(result, event);
-                }, mock.timeout);
-            }
 
-            function invokeHTTPAction(){
-                $log.debug('$mocks is not an object:', mockType, $mocks);
-                var lastArg = arguments[arguments.length - 1],
-                    callback = lastArg,
-                    mockName = arguments[0],
-                    regex = /(\w*)?}/,
-                    match = regex.exec(mockName),
-                    mockName = match[0].split('\}')[0],
-                    url = $mocks + mockName,
-                    event = {
-                        status: true
-                    };;
+                    if (typeof(callback) === 'object') {
+                        callback = arguments[arguments.length - 2];
+                    }
 
-                if (typeof(callback) === 'object') {
-                    callback = arguments[arguments.length - 2];
-                }
+                    $log.debug('http mock url:', url);
 
-                $log.debug('http mock url:',url);
-
-                $http.get(url).
-                    success(function(data,status,headers,config){
+                    $http.get(url).
+                    success(function(data) {
                         var result = data;
                         $log.debug(data);
                         setTimeout(function() {
                             callback(result, event);
                         }, 100);
-                    }). 
+                    }).
                     error(function(data, status, headers, config) {
                         $log.error(data, status, headers, config);
                     });
-                    
-            }   
 
-        }
+                };
+                if (!angular.isDefined($window.Visualforce)) {
+                    var mocker,
+                        mockType = typeof($mocks);
+                    if (mockType === 'object') {
+                        mocker = invokeStaticAction;
 
+                    } else {
 
-    }
+                        mocker = invokeHTTPAction;
+                    }
 
+                    return {
+                        remoting: {
+                            Manager: {
+                                invokeAction: mocker
+                            }
+                        }
+                    };
 
-}
-
-
-
-function jsr(jsrMocks,$q,$rootScope){
-    var Visualforce = jsrMocks;
-
-    console.log('testing', arguments, Visualforce);
-
-    return function(request) {
-        var deferred = $q.defer();
-        
-        var parameters = [request.method];
-
-        if(request.args){
-            
-            for(var i=0;i<request.args.length;i++){
-                parameters.push(request.args[i]);
-            }
-        }
-        
-        var callback = function(result, event) {
-            $rootScope.$apply(function() {
-                if (event.status) {
-                    deferred.resolve(result);
                 } else {
-                    deferred.reject(event);
+                    return Visualforce;
                 }
-            });
-        }
-        
-        parameters.push(callback);
-        
-        if(request.options){
-            parameters.push(request.options);
-        }
 
-        Visualforce.remoting.Manager.invokeAction.apply(Visualforce.remoting.Manager, parameters);
 
-        return deferred.promise;
+            }
+        };
     };
-}
 
+    var jsr = function (jsrMocks, $q, $rootScope) {
+        var Visualforce = jsrMocks;
+
+        console.log('testing', arguments, Visualforce);
+
+        return function(request) {
+            var deferred = $q.defer();
+
+            var parameters = [request.method];
+
+            if (request.args) {
+
+                for (var i = 0; i < request.args.length; i++) {
+                    parameters.push(request.args[i]);
+                }
+            }
+
+            var callback = function(result, event) {
+                $rootScope.$apply(function() {
+                    if (event.status) {
+                        deferred.resolve(result);
+                    } else {
+                        deferred.reject(event);
+                    }
+                });
+            };
+
+            parameters.push(callback);
+
+            if (request.options) {
+                parameters.push(request.options);
+            }
+
+            Visualforce.remoting.Manager.invokeAction.apply(Visualforce.remoting.Manager, parameters);
+
+            return deferred.promise;
+        };
+    };
+
+}());
